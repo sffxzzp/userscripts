6a90147ab2f108c0d5017f914304c997354d41b4
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2020-03-23 18:21:51 +0800

    fix match

--- bilibili_b2a/b2a.user.js
+++ bilibili_b2a/b2a.user.js
@@ -4,7 +4,7 @@
 // @version      0.03
 // @description  redirect bilibili's BVid to aid
 // @author       sffxzzp
-// @match        https://www.bilibili.com/video/BV*
+// @include      /https:\/\/www.bilibili.com\/video\/[Bb][Vv].*/
 // @grant        unsafeWindow
 // @icon         https://www.bilibili.com/favicon.ico
 // @updateURL    https://github.com/sffxzzp/userscripts/raw/master/bilibili_b2a/b2a.user.js
