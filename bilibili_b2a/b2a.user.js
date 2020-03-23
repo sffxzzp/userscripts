18909ac94e3fbbde0c5c120917c54a9927539e01
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2020-03-23 17:33:43 +0800

    add updateURL

--- bilibili_b2a/b2a.user.js
+++ bilibili_b2a/b2a.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Bilibili jump to av num
 // @namespace    https://github.com/sffxzzp
-// @version      0.01
+// @version      0.02
 // @description  redirect bilibili's BVid to aid
 // @author       sffxzzp
 // @match        https://www.bilibili.com/video/BV*
 // @grant        unsafeWindow
 // @icon         https://www.bilibili.com/favicon.ico
+// @updateURL    https://github.com/sffxzzp/userscripts/raw/master/bilibili_b2a/b2a.user.js
 // ==/UserScript==
 
 (function() {
