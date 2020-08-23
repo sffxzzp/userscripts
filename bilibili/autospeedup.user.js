214a239ef2c364fb380690317fba6596f69c1d54
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2020-08-23 16:08:30 +0800

    fix multiple runs

--- bilibili/autospeedup.user.js
+++ bilibili/autospeedup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili Auto SpeedUp
 // @namespace    https://github.com/sffxzzp
-// @version      0.04
+// @version      0.05
 // @description  Sets playback rate for video.
 // @author       sffxzzp
 // @match        *://www.bilibili.com/*
@@ -24,42 +24,48 @@
                 if (rec.target.classList.contains('bilibili-player-video-wrap')) {
                     var menu = rec.target.querySelector('.bilibili-player-video-btn-speed-menu');
                     var name = rec.target.querySelector('.bilibili-player-video-btn-speed-name');
-                    extraSpeeds.forEach(function (speed) {
-                        var nspeed = document.createElement('li');
-                        nspeed.className = 'bilibili-player-video-btn-speed-menu-list ';
-                        nspeed.setAttribute('data-value', speed);
-                        nspeed.innerHTML = `${parseFloat(speed).toFixed(1)}x`;
-                        menu.insertBefore(nspeed, menu.children[0]);
-                    });
-                    for (let j=0;j<menu.children.length;j++) {
-                        menu.children[j].onclick = function () {
-                            for (let k=0;k<menu.children.length;k++) {
-                                menu.children[k].classList.remove('bilibili-player-active');
-                            }
-                            this.classList.add('bilibili-player-active');
-                            player.querySelector('video').playbackRate = this.getAttribute('data-value');
-                            localStorage.setItem('auto_speedup', this.getAttribute('data-value'));
-                            if (this.getAttribute('data-value') != 1) {
-                                name.innerHTML = this.innerHTML;
-                            }
-                            else {
-                                name.innerHTML = '倍速';
-                            }
-                        }
+                    if (menu.children.length >= extraSpeeds.length + 6) {
+                        break;
                     }
-                    if (remember) {
-                        var pbrate = localStorage.getItem('auto_speedup') || 1;
-                        for (let l=0;l<menu.children.length;l++) {
-                            if (menu.children[l].getAttribute('data-value')==pbrate) {
-                                setTimeout(function () {menu.children[l].click();}, 500);
-                                break;
-                            }
-                        }
-                    }
+                    addSpeeds(menu, name);
                     break;
                 }
             }
         });
         observer.observe(player, { childList: true, subtree: true });
+    }
+    function addSpeeds(menu, name) {
+        extraSpeeds.forEach(function (speed) {
+            var nspeed = document.createElement('li');
+            nspeed.className = 'bilibili-player-video-btn-speed-menu-list ';
+            nspeed.setAttribute('data-value', speed);
+            nspeed.innerHTML = `${parseFloat(speed).toFixed(1)}x`;
+            menu.insertBefore(nspeed, menu.children[0]);
+        });
+        for (let j=0;j<menu.children.length;j++) {
+            menu.children[j].onclick = function () {
+                for (let k=0;k<menu.children.length;k++) {
+                    menu.children[k].classList.remove('bilibili-player-active');
+                }
+                this.classList.add('bilibili-player-active');
+                player.querySelector('video').playbackRate = this.getAttribute('data-value');
+                localStorage.setItem('auto_speedup', this.getAttribute('data-value'));
+                if (this.getAttribute('data-value') != 1) {
+                    name.innerHTML = this.innerHTML;
+                }
+                else {
+                    name.innerHTML = '倍速';
+                }
+            }
+        }
+        if (remember) {
+            var pbrate = localStorage.getItem('auto_speedup') || 1;
+            for (let l=0;l<menu.children.length;l++) {
+                if (menu.children[l].getAttribute('data-value')==pbrate) {
+                    setTimeout(function () {menu.children[l].click();}, 500);
+                    break;
+                }
+            }
+        }
     }
 })();
