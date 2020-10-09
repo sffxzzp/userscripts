2a657eabcca4652a9a44077e33993b8da22d4bef
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2020-10-09 19:43:30 +0800

    0.04

--- keylol/stcnsteamid.user.js
+++ keylol/stcnsteamid.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Keylol SteamID Display
 // @namespace    https://github.com/sffxzzp
-// @version      0.03
+// @version      0.04
 // @description  Display hided SteamID in keylol's steam connect bar
 // @author       sffxzzp
 // @match        *://keylol.com/t*
 // @match        *://keylol.com/forum.php?mod=viewthread&tid=*
+// @connect      api.steampowered.com
 // @connect      steamcommunity.com
 // @icon         https://keylol.com/favicon.ico
 // @grant        GM_xmlhttpRequest
@@ -13,6 +14,11 @@
 // ==/UserScript==
 
 (function() {
+    // Get data from Steam Web API.
+    var useapi = true;
+    // API Key, copied from Enhanced Steam.
+    var apikey = '824367C3B8AA3C7EADD70FF8A0DB3516';
+
     var util = (function () {
         function util() {}
         util.xhr = function (xhrData) {
@@ -38,40 +44,47 @@
         var ksd = function () {};
         ksd.prototype.setNameToLocalStorage = function (steam64id, name) {
             var nameList = JSON.parse(localStorage.getItem('ksd')) || {};
-            nameList[steam64id] = name;
+            nameList[steam64id] = {name: name, last: (new Date()).getTime()};
             localStorage.setItem('ksd', JSON.stringify(nameList));
         }
         ksd.prototype.getNameFromSteam64ID = async function (steam64id) {
             var nameList = JSON.parse(localStorage.getItem('ksd')) || {};
-            var name = nameList[steam64id];
-            if (name) {
-                return name;
+            var nameItem = nameList[steam64id];
+            if (nameItem && nameItem.last < (new Date()).getTime() - 86400000) {
+                return nameItem.name;
             }
             else {
-                var xmlData = await util.xhr({url: `https://steamcommunity.com/profiles/${steam64id}/?xml=1`});
-                xmlData = (new DOMParser).parseFromString(xmlData.body, 'text/xml');
-                name = xmlData.querySelector('steamID').textContent;
+                var name = '';
+                if (useapi) {
+                    var apiData = await util.xhr({url: `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apikey}&steamids=${steam64id}`});
+                    name = JSON.parse(apiData.body).response.players[0].personaname;
+                }
+                else {
+                    var xmlData = await util.xhr({url: `https://steamcommunity.com/profiles/${steam64id}/?xml=1`});
+                    xmlData = (new DOMParser).parseFromString(xmlData.body, 'text/xml');
+                    name = xmlData.querySelector('steamID').textContent;
+                }
                 this.setNameToLocalStorage(steam64id, name);
                 return name;
             }
         };
+        ksd.prototype.addSteamID = async function (node) {
+            var steam64id = node.querySelector('.steam_connect_user_bar_link_repcn').href.split('profiles/')[1];
+            var name = await this.getNameFromSteam64ID(steam64id);
+            node.innerHTML = `社区昵称：${name} ` + node.innerHTML;
+        };
         ksd.prototype.run = function () {
             var _this = this;
             var stbar = document.querySelectorAll('.steam_connect_user_bar') || [];
-            stbar.forEach(async function (node) {
-                var steam64id = node.querySelector('.steam_connect_user_bar_link_repcn').href.split('profiles/')[1];
-                var name = await _this.getNameFromSteam64ID(steam64id);
-                node.innerHTML = `社区昵称：${name} ` + node.innerHTML;
+            stbar.forEach(function (node) {
+                _this.addSteamID(node);
             });
             var postlist = document.getElementById("postlist");
-            var observer = new MutationObserver(async function (recs) {
+            var observer = new MutationObserver(function (recs) {
                 for (let i=0;i<recs.length;i++) {
                     let rec = recs[i];
                     if (rec.target.id.substr(0, 8) == 'postlist') {
-                        var replybar = rec.target.querySelector('.steam_connect_user_bar');
-                        var steam64id = replybar.querySelector('.steam_connect_user_bar_link_repcn').href.split('profiles/')[1];
-                        var name = await _this.getNameFromSteam64ID(steam64id);
-                        replybar.innerHTML = `社区昵称：${name} ` + replybar.innerHTML;
+                        _this.addSteamID(rec.target.querySelector('.steam_connect_user_bar'));
                         break;
                     }
                 }
