22d01c9806288fadaaa8f31da1410cf1c4aa7892
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2021-06-19 16:31:17 +0800

    0.09

--- keylol/epicinfo.user.js
+++ keylol/epicinfo.user.js
@@ -10,7 +10,7 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_removeValue
-// @version      0.08
+// @version      0.09
 // @connect      www.epicgames.com
 // @connect      store-content.ak.epicgames.com
 // @icon         https://www.epicgames.com/favicon.ico
@@ -20,12 +20,12 @@
 (function () {
     function fixNamespace(namespace) {
         for (let ns in namespace) {
-            namespace[ns] = `p(roduct)?/${namespace[ns]}`;
+            namespace[ns] = `p(roduct)?/${namespace[ns]}(/home)?$`;
         }
         namespace['85189f7cf7a64f86aa6aa91d81d36c08'] = 'bundles/borderlands-the-handsome-collection';
         namespace['bd8a7e894699493fb21503837f7b66c5'] = 'bundles/shadowrun-collection';
-        namespace['4d1607defb8840cdb849d47d012b249b'] = ['p(roduct)?/lego-batman', 'p(roduct)?/lego-batman-2', 'p(roduct)?/lego-batman-3'];
-        namespace['86ce9e7a94704e8eb1f9dbe16310e701'] = ['p(roduct)?/batman-arkham-asylum', 'p(roduct)?/batman-arkham-city', 'p(roduct)?/batman-arkham-knight'];
+        namespace['4d1607defb8840cdb849d47d012b249b'] = ['p(roduct)?/lego-batman(/home)?$', 'p(roduct)?/lego-batman-2(/home)?$', 'p(roduct)?/lego-batman-3(/home)?$'];
+        namespace['86ce9e7a94704e8eb1f9dbe16310e701'] = ['p(roduct)?/batman-arkham-asylum(/home)?$', 'p(roduct)?/batman-arkham-city(/home)?$', 'p(roduct)?/batman-arkham-knight(/home)?$'];
         return namespace;
     }
     function get(page, lastCreatedAt) {
