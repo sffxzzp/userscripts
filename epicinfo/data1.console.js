bcf3348a5e8b70508243fb42ed3cbeb021cd5690
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2022-10-21 11:51:35 +0800

    Update data1.console.js

--- epicinfo/data1.console.js
+++ epicinfo/data1.console.js
@@ -6,7 +6,6 @@ for (let i = 0; i < max; i++) {
     var url = `https://store.epicgames.com/graphql?operationName=searchStoreQuery&variables={"allowCountries":"CN","category":"games/edition/base|bundles/games","comingSoon":false,"count":100,"country":"CN","keywords":"","locale":"zh-CN","sortBy":"releaseDate","sortDir":"DESC","start":${i*100},"tag":"","withPrice":false}&extensions={"persistedQuery":{"version":1,"sha256Hash":"13a2b6787f1a20d05c75c54c78b1b8ac7c8bf4efc394edf7a5998fdf35d1adb0"}}`;
     var data = await fetch(url).then(res => res.json());
     for (let item of data.data.Catalog.searchStore.elements) {
-        if (item.namespace.length < 32) { continue; }
         console.log(item.namespace, item.urlSlug);
     }
 }
