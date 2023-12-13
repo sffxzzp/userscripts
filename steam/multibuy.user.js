e176d4a0e1497ecc291366776eb28bcebfacd5ce
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2023-12-13 16:18:03 +0800

    Update multibuy.user.js

--- steam/multibuy.user.js
+++ steam/multibuy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Multibuy Button
 // @namespace    https://github.com/sffxzzp
-// @version      0.01
+// @version      0.02
 // @description  add multibuy button to Steam badge page
 // @author       sffxzzp
 // @match        *://steamcommunity.com/*/gamecards/*/*
@@ -32,16 +32,33 @@
                 xhr.send(xhrData.data);
             });
         };
+        mb.prototype.foil = function () {
+            var foil = location.search.indexOf('border=1') > -1;
+            return foil ? 1 : 0;
+        };
         mb.prototype.getCards = async function () {
+            let foil = this.foil();
+            let appid = this.getAppID();
             let cards = [];
             // use english page to prevent name not found error.
-            let eng = await this.xhr({url: location.href.replace(location.search, '') + "?l=english"});
+            let eng = await this.xhr({url: location.href.replace(location.search, '') + "?l=english" + (foil ? "&border=1" : "")});
             let epage = (new DOMParser()).parseFromString(eng.body, 'text/html');
             epage.querySelectorAll('.badge_card_set_cards > .badge_card_set_card').forEach(node => {
                 let name = node.querySelector('.badge_card_set_text').textContent.replace(/^\s+\(\d+\)/, '').trim();
                 let count = node.querySelector('.badge_card_set_text_qty');
                 count = count ? parseInt(count.textContent.replace(/[()]/g, '')) : 0;
                 cards.push({ name: name, count: count});
+            });
+            // get hash name.
+            let market = await this.xhr({url: `https://steamcommunity.com/market/search/render/?norender=1&start=0&count=100&category_753_cardborder[]=tag_cardborder_${foil}&appid=753&category_753_Game[]=tag_app_${appid}`});
+            market = JSON.parse(market.body);
+            market.results.forEach(c => {
+                for (let card of cards) {
+                    if (c.hash_name.indexOf(card.name) > -1) {
+                        card.hash = c.hash_name;
+                        break;
+                    }
+                }
             });
             return cards;
         };
@@ -74,14 +91,13 @@
         };
         mb.prototype.init = async function () {
             let cards = await this.getCards();
-            let appid = this.getAppID();
             let clevel = this.getCurrentLevel();
             let tlevel = 5;
             let mburl = 'https://steamcommunity.com/market/multibuy?appid=753';
             for (let card of cards) {
                 let qty = tlevel-clevel-card.count;
                 qty = qty < 0 ? 0 : qty;
-                mburl += `&items[]=${appid}-${card.name}&qty[]=${qty}`;
+                mburl += `&items[]=${encodeURIComponent(card.hash)}&qty[]=${qty}`;
             }
             this.addButton(mburl);
         };
