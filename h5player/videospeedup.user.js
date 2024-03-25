659dadac8ef66e91c6448348b06fd47388a03c89
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2024-03-25 19:05:03 +0800

    Update videospeedup.user.js

--- h5player/videospeedup.user.js
+++ h5player/videospeedup.user.js
@@ -1,76 +1,74 @@
 // ==UserScript==
-// @name         Video SpeedUp
+// @name         Anime SpeedUp
 // @namespace    https://github.com/sffxzzp
-// @version      0.01
-// @description  try to extract m3u8 address from <video>.
+// @version      1.00
+// @description  Enhance experiences of anime sites.
 // @author       sffxzzp
-// @include      /^https?://(www\.age|www\.zzzfun|www\.5dm|play\.age|omofun\.tv).*/
-// @match        *://play.omofun.tv/m3u8.php?url=*
-// @include      /^https?:\/\/(www\.)?houda[sf]k\.com\/site\/hd\/resources\/views\/free\/.*/
-// @match        *://zj.jsjinfu.com*/?url=*
-// @grant        GM_openInTab
+// @match        *://www.zzzfun.one/*
+// @match        *://omofun.in/vod/play/*
+// @match        *://www.5dm.link/html5/player/*
+// @match        *://*/?url=age_*
+// @match        *://*/vip/?url=age_*
+// @grant        GM_addStyle
 // @grant        unsafeWindow
 // @updateURL    https://github.com/sffxzzp/userscripts/raw/master/h5player/videospeedup.user.js
+// @downloadURL  https://github.com/sffxzzp/userscripts/raw/master/h5player/videospeedup.user.js
 // ==/UserScript==
 
-//Function.prototype.constructor=function(){}
-
 (function() {
-    if (location.href.indexOf('omofun.tv') > 0 || location.href.indexOf('www.age') > 0 || location.href.indexOf('houda') > 0 || location.href.indexOf('zj.jsjinfu.com') > 0) {
-        document.onkeydown = function () {
-            var v = document.querySelector('video');
-            if (event.key == ">" && event.shiftKey) {
-                v.currentTime += 85;
+    GM_addStyle('.ABP-Comment-List { display: none; } .ABP-Unit .ABP-Player { width: unset; }');
+    if (location.href.indexOf('vod_play_id_') >= 0) {
+        let webPrev = document.querySelector('a.prev');
+        let webNext = document.querySelector('a.next');
+        let webFull = document.createElement('a');
+        webFull.onclick = function () {
+            let elem = document.querySelector('td#playleft > iframe');
+            if (!elem.getAttribute('isfull')) {
+                document.body.style.overflow = "hidden";
+                webPrev.style = "position: fixed; top: 20px; right: 180px; z-index: 101; margin-right: 0;";
+                webNext.style = "position: fixed; top: 20px; right: 100px; z-index: 101; margin-right: 0;";
+                this.style = "position: fixed; top: 20px; right: 0; z-index: 101; margin-right: 10px;";
+                elem.style = "position: fixed; width: 100vw; height: 100vh; left: 0; top: 0; z-index: 100;";
+                elem.setAttribute('isfull', "true");
+            } else {
+                document.body.style.overflow = "auto";
+                webPrev.style = "";
+                webNext.style = "";
+                this.style = "";
+                elem.style = "";
+                elem.setAttribute('isfull', "");
             }
-            if (event.key == "<" && event.shiftKey) {
-                v.currentTime -= 85;
-            }
-            if (event.key == ".") {
-                v.currentTime += 30;
-            }
-            if (event.key == ",") {
-                v.currentTime -= 30;
-            }
-            if (event.key == "+" && event.shiftKey) {
-                v.playbackRate += 0.5;
-            }
-            if (event.key == "_" && event.shiftKey) {
-                v.playbackRate -= 0.5;
-            }
-            if (event.key == "!" && event.shiftKey) {
-                v.playbackRate = 1;
-            }
-            if (event.key == "@" && event.shiftKey) {
-                v.playbackRate = 2;
-            }
-            if (event.key == "#" && event.shiftKey) {
-                v.playbackRate = 3;
-            }
-            if (event.key == "$" && event.shiftKey) {
-                v.playbackRate = 4;
-            }
-            if (event.key == "%" && event.shiftKey) {
-                v.playbackRate = 5;
-            }
-            if (event.key == "^" && event.shiftKey) {
-                v.playbackRate = 6;
-            }
-            if (event.key == "&" && event.shiftKey) {
-                v.playbackRate = 7;
-            }
-            if (event.key == "*" && event.shiftKey) {
-                v.playbackRate = 8;
-            }
-            console.log(`当前播放速度：${v.playbackRate}x`);
-        }
-    } else {
-        setTimeout(function () {
-            var src = document.querySelector('video').src || '';
-            if (src.indexOf('http') == 0) {
-                if (confirm(`检测到地址：${src}\n\n是否在新标签页打开？`)) {
-                    GM_openInTab('https://tools.kanojo.ml/video/?'+src, {active: true});
-                }
-            }
-        }, 3000);
+        };
+        webFull.href = "javascript:;";
+        webFull.className = "copy";
+        webFull.innerHTML = "网页全屏";
+        document.querySelector('div.p-oper').appendChild(webFull);
     }
+    document.onkeydown = function() {
+        let video = document.querySelector('video');
+        let pbrate = video.playbackRate || 1;
+        let step = 30;
+        function toggleFullscreen() {
+            var fsElement = document.querySelector('#artplayer') || document.querySelector('#player') || document.querySelector('div.ABP-Player');
+            if (!document.fullscreenElement) {
+                fsElement.requestFullscreen();
+            } else {
+                document.exitFullscreen();
+            }
+        }
+        if (event.key == ">" && event.shiftKey) {video.currentTime += step;}
+        if (event.key == "<" && event.shiftKey) {video.currentTime -= step;}
+        if (event.key == "f") { toggleFullscreen(); }
+        if (event.key == "+" && event.shiftKey) {pbrate += 0.5;}
+        if (event.key == "_" && event.shiftKey) {pbrate -= 0.5;}
+        if (event.key == "!" && event.shiftKey) {pbrate = 1;}
+        if (event.key == "@" && event.shiftKey) {pbrate = 2;}
+        if (event.key == "#" && event.shiftKey) {pbrate = 3;}
+        if (event.key == "$" && event.shiftKey) {pbrate = 4;}
+        if (event.key == "%" && event.shiftKey) {pbrate = 5;}
+        if (event.key == "^" && event.shiftKey) {pbrate = 6;}
+        if (event.key == "&" && event.shiftKey) {pbrate = 7;}
+        if (event.key == "*" && event.shiftKey) {pbrate = 8;}
+        video.playbackRate = pbrate;
+    };
 })();
