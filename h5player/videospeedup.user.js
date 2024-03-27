b41ded8b97c71d526a0d40f2dd153892a7abdff2
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2024-03-28 01:25:42 +0800

    Update videospeedup.user.js

--- h5player/videospeedup.user.js
+++ h5player/videospeedup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Anime SpeedUp
 // @namespace    https://github.com/sffxzzp
-// @version      1.00
+// @version      1.10
 // @description  Enhance experiences of anime sites.
 // @author       sffxzzp
 // @match        *://www.zzzfun.one/*
@@ -17,6 +17,11 @@
 
 (function() {
     GM_addStyle('.ABP-Comment-List { display: none; } .ABP-Unit .ABP-Player { width: unset; }');
+    if ((location.href.indexOf("www.zzzfun.one") && location.href.indexOf("/user_login.html")) >= 0) {
+        document.querySelector('#user_name').value = "sffxzzp";
+        document.querySelector('#user_pwd').value = "zzpzhx";
+        document.querySelector('#btn_submit').click();
+    }
     if (location.href.indexOf('vod_play_id_') >= 0) {
         let webPrev = document.querySelector('a.prev');
         let webNext = document.querySelector('a.next');
@@ -58,7 +63,9 @@
         }
         if (event.key == ">" && event.shiftKey) {video.currentTime += step;}
         if (event.key == "<" && event.shiftKey) {video.currentTime -= step;}
-        if (event.key == "f") { toggleFullscreen(); }
+        if (event.key == "f") {toggleFullscreen();}
+        if (event.key == "=" && event.repeat === false) {pbrate = pbrate * 2;}
+        if (event.key == "-" && event.repeat === false) {pbrate = pbrate / 2;}
         if (event.key == "+" && event.shiftKey) {pbrate += 0.5;}
         if (event.key == "_" && event.shiftKey) {pbrate -= 0.5;}
         if (event.key == "!" && event.shiftKey) {pbrate = 1;}
@@ -69,6 +76,13 @@
         if (event.key == "^" && event.shiftKey) {pbrate = 6;}
         if (event.key == "&" && event.shiftKey) {pbrate = 7;}
         if (event.key == "*" && event.shiftKey) {pbrate = 8;}
+        video.playbackRate = pbrate;
+    };
+    document.onkeyup = function() {
+        let video = document.querySelector('video');
+        let pbrate = video.playbackRate || 1;
+        if (event.key == "=") {pbrate = pbrate / 2;}
+        if (event.key == "-") {pbrate = pbrate * 2;}
         video.playbackRate = pbrate;
     };
 })();
