cef04fa3faff29498ce25f91c3c8b7b344ab7303
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2024-04-28 18:41:12 +0800

    Update wsjcn.user.js

--- other/wsjcn.user.js
+++ other/wsjcn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WSJ CN Full Text
 // @namespace    https://github.com/sffxzzp
-// @version      0.01
+// @version      0.02
 // @description  full text display of cn.wsj.com
 // @author       sffxzzp
 // @match        *://cn.wsj.com/articles/*
@@ -16,6 +16,6 @@
         location.href = location.href.replace('/articles', '/amp/articles');
     }
     if (location.href.indexOf('/amp/articles') > -1) {
-        GM_addStyle('amp-subscriptions-dialog,div.wsj-ad { display: none !important; } [subscriptions-action]:not(.i-amphtml-subs-display),[subscriptions-actions]:not(.i-amphtml-subs-display),[subscriptions-section=actions]:not(.i-amphtml-subs-display),body.i-amphtml-subs-delegated [subscriptions-section=actions],body.i-amphtml-subs-grant-unk [subscriptions-action],body.i-amphtml-subs-grant-unk [subscriptions-section=actions],body:not(.i-amphtml-subs-grant-no) [subscriptions-section=content-not-granted],body:not(.i-amphtml-subs-grant-yes) [subscriptions-section=content],body:not(.i-amphtml-subs-loading) [subscriptions-section=loading] { display: block !important; }');
+        GM_addStyle('amp-subscriptions-dialog,div.wsj-ad,div.snippet[subscriptions-section="content-not-granted"] { display: none !important; } [subscriptions-action]:not(.i-amphtml-subs-display),[subscriptions-actions]:not(.i-amphtml-subs-display),[subscriptions-section=actions]:not(.i-amphtml-subs-display),body.i-amphtml-subs-delegated [subscriptions-section=actions],body.i-amphtml-subs-grant-unk [subscriptions-action],body.i-amphtml-subs-grant-unk [subscriptions-section=actions],body:not(.i-amphtml-subs-grant-no) [subscriptions-section=content-not-granted],body:not(.i-amphtml-subs-grant-yes) [subscriptions-section=content],body:not(.i-amphtml-subs-loading) [subscriptions-section=loading] { display: block !important; }');
     }
 })();
