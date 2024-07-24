67e66a65b080d8a1ec7d600a01efc0573175bda7
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2024-07-25 00:48:24 +0800

    Add cool down time display

--- steam/steamcyberfamilynotify.user.js
+++ steam/steamcyberfamilynotify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Cyber Family Nofify
 // @namespace    https://github.com/sffxzzp
-// @version      0.21
+// @version      0.30
 // @description  show recent purchase of your steam cyber family (will exclude what you already have)
 // @author       sffxzzp
 // @match        *://*/*
@@ -41,21 +41,37 @@
     })();
     var csfn = (function () {
         var csfn = function () {};
-        csfn.prototype.getRecentList = async function () {
-            var access_token = (await util.xhr({url: 'https://store.steampowered.com/pointssummary/ajaxgetasyncconfig', type: 'json'})).body.data.webapi_token;
-            var familyData = (await util.xhr({url: `https://api.steampowered.com/IFamilyGroupsService/GetFamilyGroupForUser/v1/?include_family_group_response=true&access_token=${access_token}`, type: 'json'})).body.response;
-            var family_groupid = familyData.family_groupid;
+        csfn.prototype.getAccessToken = async function () {
+            this.access_token = (await util.xhr({url: 'https://store.steampowered.com/pointssummary/ajaxgetasyncconfig', type: 'json'})).body.data.webapi_token;
+        };
+        csfn.prototype.getFamilyData = async function () {
+            return (await util.xhr({url: `https://api.steampowered.com/IFamilyGroupsService/GetFamilyGroupForUser/v1/?include_family_group_response=true&access_token=${this.access_token}`, type: 'json'})).body.response;
+        };
+        csfn.prototype.getMemberData = async function (reqStr) {
+            return (await util.xhr({url: `https://api.steampowered.com/IPlayerService/GetPlayerLinkDetails/v1/?access_token=${this.access_token}${reqStr}`, type: 'json'})).body.response.accounts;
+        };
+        csfn.prototype.getGameList = async function (family_groupid) {
+            return (await util.xhr({url: `https://api.steampowered.com/IFamilyGroupsService/GetSharedLibraryApps/v1/?family_groupid=${family_groupid}&include_own=true&access_token=${this.access_token}`, type: 'json'})).body.response;
+        };
+        csfn.prototype.getMembers = async function (members) {
             var family_members = {};
             var reqStr = "";
-            for (let i = 0; i < familyData.family_group.members.length; i++) {
-                family_members[familyData.family_group.members[i].steamid] = "";
-                reqStr += "&steamids["+i+"]="+familyData.family_group.members[i].steamid;
+            for (let i = 0; i < members.length; i++) {
+                family_members[members[i].steamid] = "";
+                reqStr += "&steamids["+i+"]="+members[i].steamid;
             }
-            var memberData = (await util.xhr({url: `https://api.steampowered.com/IPlayerService/GetPlayerLinkDetails/v1/?access_token=${access_token}${reqStr}`, type: 'json'})).body.response.accounts;
+            var memberData = await this.getMemberData(reqStr);
             memberData.forEach(function (member) {
                 family_members[member.public_data.steamid] = member.public_data.persona_name;
             });
-            var gameList = (await util.xhr({url: `https://api.steampowered.com/IFamilyGroupsService/GetSharedLibraryApps/v1/?family_groupid=${family_groupid}&include_own=true&access_token=${access_token}`, type: 'json'})).body.response;
+            return family_members;
+        };
+        csfn.prototype.getRecentList = async function () {
+            await this.getAccessToken();
+            var familyData = await this.getFamilyData();
+            var family_groupid = familyData.family_groupid;
+            var family_members = await this.getMembers(familyData.family_group.members);
+            var gameList = await this.getGameList(family_groupid);
             var steamid = gameList.owner_steamid;
             var tmpList = [];
             gameList.apps.forEach(function (game) {
@@ -75,19 +91,80 @@
             tmpList.sort(function (a, b) { return b.time - a.time });
             return tmpList.slice(0, 99);
         };
+        csfn.prototype.secondsDisplay = function (seconds) {
+            if (seconds <= 0) {
+                return "无冷却时间";
+            }
+            var outStr = "冷却时间：";
+            var leftSeconds = seconds;
+            var days = parseInt(leftSeconds / 86400);
+            leftSeconds %= 86400;
+            if (days > 0) {
+                outStr += days + " 天 ";
+            }
+            var hours = parseInt(leftSeconds / 3600);
+            leftSeconds %= 3600;
+            if (hours > 0) {
+                outStr += hours + " 小时 ";
+            }
+            return outStr;
+        };
+        csfn.prototype.displayCoolDown = async function () {
+            var _this = this;
+            var cdData = GM_getValue("cddata") || null;
+            var currentTime = parseInt((new Date()).getTime() / 1000);
+            if (cdData == null) {
+                await this.getAccessToken();
+                var familyData = await this.getFamilyData();
+                var family_groupid = familyData.family_groupid;
+                var family_members = await this.getMembers(familyData.family_group.members);
+                cdData = {};
+                for (let i = 0; i < familyData.family_group.members.length; i++) {
+                    cdData[family_members[familyData.family_group.members[i].steamid]] = {
+                        steamid: familyData.family_group.members[i].steamid,
+                        cooldown: familyData.family_group.members[i].cooldown_seconds_remaining == 0 ? 0 : familyData.family_group.members[i].cooldown_seconds_remaining + currentTime,
+                    };
+                }
+                GM_setValue("cddata", cdData);
+            }
+            document.querySelectorAll('div.avatarHolder ~ div').forEach(function (node) {
+                var nickname = node.querySelector('div > div > div').innerHTML;
+                var cdDiv = document.createElement('div');
+                cdDiv.style = "font-size: 14px; color: darkgray";
+                cdDiv.innerHTML = _this.secondsDisplay(cdData[nickname].cooldown - currentTime);
+                node.appendChild(cdDiv);
+            });
+        };
+        csfn.prototype.coolDown = function () {
+            var _this = this;
+            var target = document.querySelector('div[data-featuretarget="family-management"]');
+            var observer = new MutationObserver(function (recs) {
+                for (let i=0;i<recs.length;i++) {
+                    let rec = recs[i];
+                    if (rec.target.querySelector('div.avatarHolder') != null) {
+                        setTimeout(function () {_this.displayCoolDown()}, 500);
+                        break;
+                    }
+                }
+            });
+            observer.observe(target, { childList: true, subtree: true });
+        };
         csfn.prototype.run = async function () {
+            if (location.href.indexOf('https://store.steampowered.com/account/familymanagement')>-1) {
+                this.coolDown();
+            }
             var lastcheck = GM_getValue('time') || 0;
             var lastrun = GM_getValue('lastrun') || 0;
-            var timeCond = (new Date()).getTime() - 79200000;
+            var timeCond = (new Date()).getTime() - 43200000;
             var rTimeCond = (new Date()).getTime() - 1800000;
             if (lastcheck < timeCond && lastrun < rTimeCond) {
+                GM_setValue('lastrun', (new Date()).getTime());
                 var oldList = GM_getValue('list') || [];
                 var oldIdList = [];
                 oldList.forEach(function (game) {
                     oldIdList.push(game.appid);
                 });
                 var newList = [];
-                GM_setValue('lastrun', (new Date()).getTime());
                 try {
                     newList = await this.getRecentList();
                 }
