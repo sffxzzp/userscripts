e8fa11315965df65b8859e8b7895d4e166891e6a
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2025-11-07 13:44:45 -0700

    Store Video Download matches wide view of Steam Store

--- steam/storevideodownloader.user.js
+++ steam/storevideodownloader.user.js
@@ -1,36 +1,65 @@
 // ==UserScript==
 // @name         Steam Store Video Downloader
 // @namespace    https://github.com/sffxzzp
-// @version      0.11
+// @version      0.20
 // @description  add download button in store page to get videos.
 // @author       sffxzzp
 // @match        *://store.steampowered.com/app/*
 // @icon         https://store.steampowered.com/favicon.ico
-// @grant        none
+// @grant        unsafeWindow
 // @updateURL    https://github.com/sffxzzp/userscripts/raw/master/steam/storevideodownloader.user.js
 // @downloadURL  https://github.com/sffxzzp/userscripts/raw/master/steam/storevideodownloader.user.js
 // ==/UserScript==
 
 (function() {
-    document.querySelectorAll('div#highlight_strip_scroll > div.highlight_strip_movie').forEach(function (node) {
-        let assetId = parseInt(node.id.split('_')[2]);
-        let movie = document.querySelector(`div#highlight_movie_${assetId}`);
-        let mUrls = JSON.parse(movie.dataset.props).dashManifests;
-        let mUrl = '';
-        let found264 = false;
-        for (let u of mUrls) {
-            if (u.indexOf('h264') > 0) {
-                mUrl = u;
-                found264 = true;
-            }
-        }
-        if (found264 == false) {
-            mUrl = mUrls[0];
-        }
-        if (mUrl) {
-            node.ondblclick = function () {
-                prompt("url: ", mUrl);
-            }
+    var carousel = document.querySelector("div.gamehighlight_desktopcarousel");
+    var observer = new MutationObserver(function (recs) {
+        if (carousel.children[0].children.length == 4) {
+            loadBtn();
         }
     });
+    observer.observe(carousel, { childList: true, subtree: true });
+
+    function loadBtn() {
+        if (unsafeWindow.ssvdLoaded) {
+            return;
+        }
+        unsafeWindow.ssvdLoaded = true;
+        let trailers = JSON.parse(document.querySelector('div.gamehighlight_desktopcarousel').dataset.props).trailers;
+        document.querySelectorAll('div.gamehighlight_desktopcarousel >div > div:last-child > div:first-child > div:has(svg.SVGIcon_Button)').forEach(function (node) {
+            let imgSrc = node.querySelector('img').src;
+            let mUrl = '';
+            let found264 = false;
+            let target;
+            for (let trailer of trailers) {
+                if (trailer.thumbnail == imgSrc) {
+                    target = trailer;
+                }
+            }
+            if (target) {
+                for (let u of target.dashManifests) {
+                    if (u.indexOf('h264') > 0) {
+                        mUrl = u;
+                        found264 = true;
+                    }
+                }
+                if (found264 == false) {
+                    mUrl = target.dashManifests[0];
+                }
+                if (mUrl) {
+                    node.ondblclick = function () {
+                        prompt("url: ", mUrl);
+                    }
+                } else {
+                    node.ondblclick = function () {
+                        alert('no url found.');
+                    }
+                }
+            } else {
+                node.ondblclick = function () {
+                    alert('no url found.')
+                }
+            }
+        });
+    }
 })();
