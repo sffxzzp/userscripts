4155bd84b56d0a61d9a82efc6ef4c59f47e931b5
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2026-08-20 23:40:30 -0600

    Bump version from 0.03 to 0.04

--- keylol/steamignoredgames.user.js
+++ keylol/steamignoredgames.user.js
@@ -5,7 +5,7 @@
 // @description  快速匹配已忽略 Steam 游戏信息
 // @match        *://keylol.com/*
 // @grant        GM_getResourceText
-// @version      0.03
+// @version      0.04
 // @resource     userdata https://store.steampowered.com/dynamicstore/userdata
 // @icon         https://store.steampowered.com/favicon.ico
 // @updateURL    https://github.com/sffxzzp/userscripts/raw/master/keylol/steamignoredgames.user.js
@@ -21,7 +21,7 @@
     document.querySelectorAll('[id^=pid] a').forEach(function (a) {
         if (a.href.indexOf('store.steampowered.com')>-1) {
             for (var app of iApps) {
-                if (a.href.indexOf(app)>-1 && !(a.classList.contains("steam-info-own") && a.classList.contains("steam-info-wish")) && a.style.backgroundColor=="") {
+                if (new RegExp('/app/'+app +'(/|$)').test(a.href) && !(a.classList.contains("steam-info-own") && a.classList.contains("steam-info-wish")) && a.style.backgroundColor=="") {
                     a.style = 'background-color: gray; color: white;';
                 }
             }
